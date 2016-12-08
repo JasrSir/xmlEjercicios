abd1028f0397306ea3dc47e46d9728b4ff8b9827
Juan Antonio Suarez <jasrsir@gmail.com>
2016-12-08 15:52:21 +0100

    ya arreglao

---
+++ app/src/main/java/com/jasrsir/xmljasr/EstacionSeleccionada.java
@@ -0,0 +1,13 @@
+package com.jasrsir.xmljasr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EstacionSeleccionada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estacion_seleccionada);
+    }
+}
--- app/src/main/java/com/jasrsir/xmljasr/clases/Bizi.java
+++ app/src/main/java/com/jasrsir/xmljasr/clases/Bizi.java
@@ -10,4 +10,44 @@ public class Bizi {
     private String estado;
     private String bicisDisponibles;
     private String anclajesDisponibles;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBicisDisponibles() {
+        return bicisDisponibles;
+    }
+
+    public void setBicisDisponibles(String bicisDisponibles) {
+        this.bicisDisponibles = bicisDisponibles;
+    }
+
+    public String getAnclajesDisponibles() {
+        return anclajesDisponibles;
+    }
+
+    public void setAnclajesDisponibles(String anclajesDisponibles) {
+        this.anclajesDisponibles = anclajesDisponibles;
+    }
 }
